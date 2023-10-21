5e9a99d28875b47ddef9b33340cd94f3376c03a0
delighted <deborahdelighted5@gmail.com>
2023-10-21 16:08:29 +0100

    Chapter Six

--- src/CardValidatorAndShoppingList/StudentGrade.java
+++ src/CardValidatorAndShoppingList/StudentGrade.java
@@ -8,9 +8,7 @@ public class StudentGrade {
     static int numberOfSubjects = 0;
 
     public static void main(String[] args) {
-        // System.out.println(Arrays.deepToString(enterScore()));
-        //  printScores();
-        //subjectSummary();
+        printAll();
 
     }
 
@@ -32,7 +30,6 @@ public class StudentGrade {
         int[][] scores = new int[numberOfStudents][numberOfSubjects];
         int[] numOfStudents = new int[numberOfStudents];
         int[] numOfSubjects = new int[numberOfSubjects];
-        int[] total = new int[numberOfSubjects];
         for (int students = 0; students < numOfStudents.length; students++) {
             for (int subjects = 0; subjects < numOfSubjects.length; subjects++) {
                 System.out.printf("""
@@ -48,33 +45,97 @@ public class StudentGrade {
         return scores;
     }
 
-    public static void printScores() {
+    public static void printScores(int [] [] enterScore) {
         int sum = 0;
         int total = 0;
         double average = 0.0;
         int count = 0;
-        int[][] newScores = enterScore();
+
         System.out.print("STUDENTS");
-        for (int num = 0; num < numberOfSubjects; num++) {
+
+            for(int num = 0; num < enterScore[0].length; num++){
             System.out.printf("%10s%d", "SUB", (num + 1));
         }
         System.out.printf("%10s%10s%10s%n", "TOT", "AVE", "POS");
-        for (int student = 0; student < newScores.length; student++) {
+
+          for(int student = 0; student < enterScore.length; student++){
             System.out.printf("%s%d", "STUDENT", (student + 1));
-            for (int subject = 0; subject < newScores[student].length; subject++) {
-                total = newScores[student][subject];
+            for (int subject = 0; subject < enterScore[student].length; subject++) {
+                total = enterScore[student][subject];
                 sum += total;
                 count++;
-                System.out.printf("%11d", newScores[student][subject]);
+                System.out.printf("%11d",enterScore[student][subject]);
             }
-            average = (double) sum / newScores[student].length;
+            average = (double) sum / enterScore[student].length;
             System.out.printf("%11d", sum);
             System.out.printf("%11.2f", average);
             System.out.println();
             sum = 0;
-            // average = 0;
         }
     }
 
+    public static void subjectSummary(int [] [] enterScore) {
 
+        int max = enterScore[0][0];
+        int min = enterScore[0][0];
+        int maxIndex = 1;
+            int minIndex = 1;
+        int sum = 0;
+        int passCount = 0;
+        int failCount = 0;
+        double average = 0.0;
+        System.out.println("SUBJECT SUMMARY");
+        for (int subject = 0; subject < enterScore[0].length; subject++) {
+             max = enterScore[0][subject];
+             min = enterScore[0][subject];
+            for (int student = 0; student < enterScore.length; student++) {
+
+                sum += enterScore[student][subject];
+                average = (double) sum / enterScore.length;
+
+
+                if (enterScore[student][subject] > max) {
+                    max = enterScore[student][subject];
+                }
+                    maxIndex += student;
+
+                if (enterScore[student][subject] < min) {
+                    min = enterScore[student][subject];
+                    minIndex += student ;
+                }
+                if (enterScore[student][subject] >= 50) {
+                    passCount++;
+                } else {
+                    failCount++;
+
+                }
+
+            }
+
+            System.out.printf("""
+                    Subject %d
+                    Highest scoring student is : Student %d scoring %d
+                    Lowest Scoring student is : Student %d scoring %d
+                    Total Score is: %d
+                    Average score is : %.2f
+                    Number of passes : %d
+                    Number of fail: %d %n""", (subject + 1), maxIndex, max, minIndex, min, sum, average, passCount, failCount);
+
+            maxIndex = 1;
+            minIndex = 1;
+            sum = 0;
+            passCount = 0;
+            failCount = 0;
+        }
+
+    }
+
+public static void printAll(){
+      int [] [] scores = enterScore();
+      printScores(scores);
+      subjectSummary(scores);
 }
+}
+
+
+
--- src/ChapterSix/ComputerAssistedInstruction.java
+++ src/ChapterSix/ComputerAssistedInstruction.java
@@ -6,38 +6,48 @@ import java.util.Scanner;
 public class ComputerAssistedInstruction {
     static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
-
         promptStudent();
 
 
     }
-
-
-    public static int promptStudent() {
+    public static void promptStudent() {
         SecureRandom secureRandom = new SecureRandom();
         int firstTimesNumber = secureRandom.nextInt(9);
         int secondTimesNumber = secureRandom.nextInt(9);
-        System.out.println("How much is " + firstTimesNumber + " times " + secondTimesNumber);
-        return firstTimesNumber * secondTimesNumber;
-    }
-    public static void checkAnswer(int studentAnswer) {
-        studentAnswer = scanner.nextInt();
-        int answer = promptStudent();
-        studentAnswer = scanner.nextInt();
+        print("How much is " + firstTimesNumber + " times " + secondTimesNumber);
+        int answer = firstTimesNumber * secondTimesNumber;
+        int studentAnswer = scanner.nextInt();
         while (studentAnswer != answer) {
-            System.out.println("No. Please try again");
+            print("No. Please try again");
+            print("How much is " + firstTimesNumber + " times " + secondTimesNumber);
             studentAnswer = scanner.nextInt();
         }
-        if (studentAnswer == answer) {
-            System.out.println("Very Good");
+        if (studentAnswer == answer){
+            print("Very Good");
+            generateQuestions();
         }
 
     }
-
-    public static void generateQuestions(int studentAnswer) {
-        promptStudent();
-        checkAnswer(studentAnswer);
+    public static void generateQuestions() {
+        SecureRandom random = new SecureRandom();
+        int firstNumber = random.nextInt(9);
+        int secondNumber = random.nextInt(9);
+        print("How much is " + firstNumber + " times " + secondNumber);
+        int randomAnswer = firstNumber * secondNumber;
+        int studentAnswer = scanner.nextInt();
+        while (studentAnswer != randomAnswer) {
+            print("No. Please try again");
+            print("How much is " + firstNumber + " times " + secondNumber);
+            studentAnswer = scanner.nextInt();
+        }
+        if (studentAnswer == randomAnswer){
+            generateQuestions();
+        }
     }
 
+
+public static void print(String input){
+    System.out.println(input);
+}
 }
 
