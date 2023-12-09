25703da7775d962efd96b4d10680f2a312772d09
delighted <deborahdelighted5@gmail.com>
2023-12-09 20:46:35 +0100

    mmm

--- test/ArrayExecise/SortedStringTest.java
+++ test/ArrayExecise/SortedStringTest.java
@@ -5,18 +5,18 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class SortedStringTest {
-//    @Test
-//    public void testThatFunctionCanSortUnSortedString(){
-//        String letters = "acb";
-//        String result = SortedString.sortStrings(letters);
-//        assertEquals("abc",result);
-//    }
-//    @Test public void whenTheWordAreSortedTest(){
-//        String letters = "abc";
-//        String result = SortedString.sortStrings(letters);
-//        assertEquals("abc",result);
-//
-//    }
+    @Test
+    public void testThatFunctionCanSortUnSortedString(){
+        String letters = "acb";
+        String result = SortedString.sortStrings(letters);
+        assertEquals("abc",result);
+    }
+    @Test public void whenTheWordAreSortedTest(){
+        String letters = "abc";
+        String result = SortedString.sortStrings(letters);
+        assertEquals("abc",result);
+
+    }
     @Test public void whenWordAreNotAlphabeticallyInorderTest(){
         String letters = "debby";
         String result = SortedString.sortStrings(letters);
